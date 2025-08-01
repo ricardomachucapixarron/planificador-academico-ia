a291346a0562ba6aadecbcf9adc4bf98ecfa6a63
ricardomachucapixarron <105318092+ricardomachucapixarron@users.noreply.github.com>
2025-08-01 01:57:48 -0400

    Build: Sobrescribe regla no-explicit-any a warn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // AGREGA ESTE NUEVO OBJETO AQUÍ ABAJO
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
